13bfd92e2439306e392f5274e75c84149118dfdc
vunguyen-appvity <105355883+vunguyen-appvity@users.noreply.github.com>
2022-08-31 03:15:34 -0400

    fixed conflict + update menu footer
    - create menu
    - create footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, WelcomeComponent, ErrorComponent, ListTodosComponent],
+  declarations: [AppComponent, LoginComponent, WelcomeComponent, ErrorComponent, ListTodosComponent, MenuComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
