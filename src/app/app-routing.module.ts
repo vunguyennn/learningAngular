5b67519766096ec1e5b33ce093fa10c429a9da8c
vunguyen-appvity <105355883+vunguyen-appvity@users.noreply.github.com>
2022-09-21 10:05:19 +0700

    route-guard #16
    - Do not allow user access to any page if not login
    - If the user try to access with URL, navigate them to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteGuardService } from '@pendo/services';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -8,11 +9,15 @@ import { WelcomeComponent } from './welcome/character.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [RouteGuardService] },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'character', component: WelcomeComponent },
-  { path: '**', component: ErrorComponent },
+  {
+    path: 'character',
+    component: WelcomeComponent,
+    canActivate: [RouteGuardService],
+  },
+  { path: '**', component: ErrorComponent, canActivate: [RouteGuardService] },
 ];
 
 @NgModule({
